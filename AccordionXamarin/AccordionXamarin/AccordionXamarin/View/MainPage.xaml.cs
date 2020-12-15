344d58074719cda2caf6e8a7ca9f15395889167e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-15 12:54:50 +0530

    XAMARIN-34513 - Sample and screenshot added

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/View/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI.XamForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public partial class MainPage : ReactiveContentPage<ViewModel>
+    {
+        public MainPage(ViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}
